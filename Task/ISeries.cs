3fa24f8b339ecc7909dfae7c232793a34907a93a
lllypik <blesphoton@gmail.com>
2022-07-22 17:21:59 +0300

    Добавьте файлы проекта.

---
+++ Task/ISeries.cs
@@ -0,0 +1,75 @@
+﻿namespace Task
+{
+    //Интерфейс ввода данных
+    interface ISeries
+    {
+        void setStart(int x);
+        int getNext();
+        void reset();
+    }
+
+    //Класс арифметической прогрессии
+    class ArithProgression : ISeries
+    {
+        int beginValue;
+        int nextValue;
+        int step;
+
+        //метод получения очередного значения
+        public int getNext()
+        {
+            nextValue += step;
+            return nextValue;
+        }
+
+        //метод сброса прогрессии к начальному значению
+        public void reset()
+        {
+            nextValue = beginValue;
+        }
+
+        //метод установки начального значения
+        public void setStart(int x)
+        {
+            beginValue = x;
+            nextValue = beginValue;
+        }
+        //метод установки начального шага
+        public void setStep(int step)
+        {
+            this.step = step;
+        }
+    }
+    //Класс геометрической прогрессии
+    class GeomProgression : ISeries
+    {
+        int beginValue;
+        int nextValue;
+        int step;
+
+        //метод получения очередного значения
+        public int getNext()
+        {
+            nextValue *= step;
+            return nextValue;
+        }
+
+        //метод сброса прогрессии к начальному значению
+        public void reset()
+        {
+            nextValue = beginValue;
+        }
+
+        //метод установки начального значения
+        public void setStart(int x)
+        {
+            beginValue = x;
+            nextValue = beginValue;
+        }
+        //метод установки начального шага
+        public void setStep(int step)
+        {
+            this.step = step;
+        }
+    }
+}
---
+++ Task/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Проверка работы метода алгоритмической прогрессии
+            //Задаем начальные параметры
+            ArithProgression arith = new ArithProgression();
+            arith.setStart(2);
+            arith.setStep(10);
+
+            //Выводим результат
+            Console.WriteLine("Вывод значений арифметичской прогрессии");
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine(arith.getNext());
+            }
+
+            //Сбрасываем начальное значение
+            arith.reset();
+            //Проверямем сброс значений до заданного и работу метода в дальнейшем
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine(arith.getNext());
+            }
+            Console.ReadKey();
+
+            // Проверка работы метода алгоритмической прогрессии
+            //Задаем начальные параметры
+            GeomProgression geom = new GeomProgression();
+            geom.setStart(13);
+            geom.setStep(6);
+
+            //Выводим результат
+            Console.WriteLine("Вывод значений арифметичской прогрессии");
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine(geom.getNext());
+            }
+
+            //Сбрасываем начальное значение
+            geom.reset();
+
+            //Проверямем сброс значений до заданного и работу метода в дальнейшем
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine(geom.getNext());
+            }
+            Console.ReadKey();
+
+        }
+    }
+
+
+}
